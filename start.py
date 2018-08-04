48f2c7d4c08330ec08e123f01ffc13052d9e4561
Harry Williams <harry@Harrys-MacBook-Pro.local>
2018-08-04 09:26:31 +0100

    Created fundamental playable game start to finish, lacks many features

--- start.py
+++ start.py
@@ -8,10 +8,13 @@ class Game:
         """
         Begins the game ready for playing
         """
+        self.guess_count = 0
+        self.guessed = False
         self.player_1 = input('Who is going to be creating the key? (name) ')
         self.player_2 = input('Who is going to be guessing? (name) ')
         self.chosen_code = input(self.player_1 + ' please enter your secret key.\n')
-        # TODO guess()
+        
+        
 
     def __init__(self):
         """
@@ -21,7 +24,27 @@ class Game:
         self.player_1 = ''
         self.player_2 = ''
 
+    def guess(self):
+        """
+        Allow player 2 to perform their guess
+        """
+        key_guess = input(self.player_2 + ' please enter your 4 digit guess: \n')
+        self.guess_count += 1
+        return self.check(key_guess)
+
+    def check(self, key_guess):
+        """
+        Checks if current guess is right
+        """
+        if(key_guess == self.chosen_code):
+            return True
+        
+        return False
+
 
 if __name__ == "__main__":
     g = Game()
     g.start()
+    while not g.guessed:
+        g.guessed = g.guess()
+    print('\nCongratulations {0} you guessed the key correctly in {1} guesses, it was: {2}\n'.format(g.player_2, g.guess_count, g.chosen_code) )
